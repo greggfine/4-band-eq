0a4a1eef9c588872e7ca92c618d5f6b6cf605af0
Gregg Fine <bbeez8500@gmail.com>
2018-01-31 16:52:36 -0500

    Improved Parameter Tuning
    Next step is to improve param tuning further.

--- main.js
+++ main.js
@@ -20,6 +20,22 @@ const highShelfFreqSlider = document.querySelector("#high-shelf-freq");
 
 const masterVolumeSlider = document.querySelector("#master-volume-slider");
 
+const lowShelfGainVal = document.querySelector("#low-shelf-gain-val");
+const lowShelfFreqVal = document.querySelector("#low-shelf-freq-val");
+
+const highShelfGainVal = document.querySelector("#high-shelf-gain-val");
+const highShelfFreqVal = document.querySelector("#high-shelf-freq-val");
+
+const peak1GainVal = document.querySelector("#peak1-gain-val");
+const peak1FreqVal = document.querySelector("#peak1-freq-val");
+const peak1QVal = document.querySelector("#peak1-Q-val");
+
+const peak2GainVal = document.querySelector("#peak2-gain-val");
+const peak2FreqVal = document.querySelector("#peak2-freq-val");
+const peak2QVal = document.querySelector("#peak2-Q-val");
+
+const masterVolVal = document.querySelector("#masterVol-gain-val")
+
 playBtn.addEventListener("click", play);
 stopBtn.addEventListener("click", stop);
 
@@ -39,71 +55,87 @@ const ctx = new (window.AudioContext || window.webkitAudioContext)();
 
 function play() {
 	playSound = ctx.createBufferSource();
-	playSound.buffer = audioBuffer;
-
-	
+	playSound.buffer = audioBuffer;	
 
 	//Low-Shelf
 	const lowShelf = ctx.createBiquadFilter();
 	lowShelf.type = "lowshelf";
+	lowShelf.frequency.value = 1000;
+	lowShelf.gain.value = 8;
 
 	lowShelfFreqSlider.addEventListener("change", () => {	
-		lowShelf.frequency.value = lowShelfFreqSlider.value * 100;
+		lowShelf.frequency.value = lowShelfFreqSlider.value;
+		lowShelfFreqVal.textContent = Math.round(lowShelf.frequency.value);
 	})
 
 	lowShelfGainSlider.addEventListener("change", () => {
 		lowShelf.gain.value = lowShelfGainSlider.value;
+		lowShelfGainVal.textContent = lowShelf.gain.value.toFixed(2);
 	})
 
 	//Peak1
 	const peak1 = ctx.createBiquadFilter();
 	peak1.type = "peaking";
+	peak1.gain.value = 0.50;
+	peak1.Q.value = 0.50;
+	peak1.frequency.value = 1000;
 
-	peak1FreqSlider.addEventListener("change", () => {	
-		peak1.frequency.value = peak1FreqSlider.value * 100;
+	peak1FreqSlider.addEventListener("change", () => {
+		peak1.frequency.value = peak1FreqSlider.value;
+		peak1FreqVal.textContent = Math.round(peak1.frequency.value);
 	})
 
 	peak1GainSlider.addEventListener("change", () => {
 		peak1.gain.value = peak1GainSlider.value;
+		peak1GainVal.textContent = peak1.gain.value.toFixed(2);
 	})
 
 	peak1QSlider.addEventListener("change", () => {
 		peak1.Q.value = peak1QSlider.value;
+		peak1QVal.textContent = peak1.Q.value.toFixed(2);
 	})
-
 
 	//Peak2
 	const peak2 = ctx.createBiquadFilter();
 	peak2.type = "peaking";
+	peak2.type = "peaking";
+	peak2.gain.value = 0.50;
+	peak2.Q.value = 0.50;
+	peak2.frequency.value = 1000;
 
 	peak2FreqSlider.addEventListener("change", () => {	
-		peak2.frequency.value = peak2FreqSlider.value * 100;
+		peak2.frequency.value = peak2FreqSlider.value;
+		peak2FreqVal.textContent = Math.round(peak2.frequency.value);
 	})
 
 	peak2GainSlider.addEventListener("change", () => {
 		peak2.gain.value = peak2GainSlider.value;
+		peak2GainVal.textContent = peak2.gain.value.toFixed(2);
 	})
 
 	peak2QSlider.addEventListener("change", () => {
 		peak2.Q.value = peak2QSlider.value;
+		peak2QVal.textContent = peak2.Q.value.toFixed(2);
 	})
-
-
 
 	//High-Shelf
 	const highShelf = ctx.createBiquadFilter();
 	highShelf.type = "highshelf";
+	highShelf.frequency.value = 1000;
+	highShelf.gain.value = 8;
 
 	highShelfFreqSlider.addEventListener("change", () => {	
-		highShelf.frequency.value = highShelfFreqSlider.value * 100;
+		highShelf.frequency.value = highShelfFreqSlider.value;
+		highShelfFreqVal.textContent = Math.round(highShelf.frequency.value);
 	})
 
 	highShelfGainSlider.addEventListener("change", () => {
 		highShelf.gain.value = highShelfGainSlider.value;
+		highShelfGainVal.textContent = highShelf.gain.value.toFixed(2);
 	})
 
 	let volume = ctx.createGain();
-	
+	volume.gain.value = .25;
 
 	playSound.connect(lowShelf);
 	playSound.connect(highShelf);
@@ -118,11 +150,10 @@ function play() {
 
 	masterVolumeSlider.addEventListener("change", () => {
 		volume.gain.value = masterVolumeSlider.value;
+		masterVolVal.textContent = volume.gain.value.toFixed(2);
 	})
 }
 
 function stop() {
 	playSound.stop(ctx.currentTime);
 }
-
-//Create 1band lowshelf with gain and freq
